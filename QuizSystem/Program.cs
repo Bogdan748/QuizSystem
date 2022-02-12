e2cf6ed1d92f4f3bcf7579f3920d1ae5a71e7a73
Bogdan748 <andriesbogdan00@gmail.com>
2022-02-12 11:13:23 +0200

    Add project files.

---
+++ QuizSystem/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.IO;
+using QuizSystem;
+
+namespace QuizSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (TextFieldParser parser = new TextFieldParser("D:\\Fast Track IT\\Github4\\QuizSystem\\QuizSystem\\Quiz.csv"))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                int itemCount = 0;
+                while (!parser.EndOfData)
+                {
+                    
+                    bool firstLine = true;
+                    //Process row
+                    string[] fields = parser.ReadFields();
+                    foreach (string field in fields)
+                    {
+                        if (firstLine)//ignora primul rand
+                        {
+                            firstLine = false;
+                            continue;
+                        }
+
+                        QuizItems items = new QuizItems();
+                        //TODO: Process field
+                        itemCount += 1;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ QuizSystem/QuizItems.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizSystem
+{
+    public class QuizIndexer
+    {
+        private List<QuizItem> list = new List<QuizItem>();
+        
+        public QuizIndexer(QuizItem item)
+        {
+            this.list.Add(item);
+        }
+
+        public QuizItem this[int index]
+        {
+            get { return this.list[index]; }
+            set { this.list[index] = value; }
+        }
+    }
+
+    public class QuizItem
+    {
+        private string Question;
+        private Dictionary<string, bool> Answers= new Dictionary<string, bool>();
+        
+
+        public QuizItem(string question, string[] answers, bool[] correct )
+        {
+            this.Question = question;
+            
+            for (int i=0; i< answers.Length; i++)
+            {
+                this.Answers.Add(answers[i], correct[i]);
+            }
+
+        }
+    }
+}
